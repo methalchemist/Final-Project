c07341af60821a57eda7c2391819309e67478215
METH <shahzaibx2@gmail.com>
2022-05-18 16:35:37 -0400

    added weather api

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //add api calls and stuff
+let weather = {
+    apiKey: "328591a3004546ba6ef2e4b92cf93390",
+    getWeather: function(zip) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?zip=" 
+        + zip 
+        +",&units=imperial&appid=" 
+        + this.apiKey)
+        .then((response) => response.json())
+        .then((data) => console.log(data)); 
+    },
+    displayWeather: function(data) {
+        //stuff
+    }
+}

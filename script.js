d80dc471bb3352b9c8f9e1277387cab388dd402b
METH <shahzaibx2@gmail.com>
2022-05-18 16:50:54 -0400

    added the link

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 //add api calls and stuff
+// api call: https://api.openweathermap.org/data/2.5/weather?zip=11365,us&units=imperial&appid=328591a3004546ba6ef2e4b92cf93390
 let weather = {
     apiKey: "328591a3004546ba6ef2e4b92cf93390",
     getWeather: function(zip) {
